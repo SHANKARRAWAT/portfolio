249a437ae87e788297096fa4eae206e8911d574f
Shankar_Rawat <90569452+SHANKARRAWAT@users.noreply.github.com>
2024-01-25 10:15:25 +0530

    update

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -2,11 +2,12 @@ import './WorkCardStyle.css'
 // import '../index.css'
 import React from 'react'
 import news from '../assests/news.jpg'
+
 import { NavLink } from 'react-router-dom'
 const WorkCard = (props) => {
   return (
 
-    <div className="project-container">
+    // <div className="project-container">
     <div className="project-card">
     <img src={props.imgsrc} alt="img not found"  />
         <h2 className='project-title'>
@@ -24,7 +25,7 @@ const WorkCard = (props) => {
     </div>
     </div>
 
- </div>
+//  </div>
   )
 }
 
