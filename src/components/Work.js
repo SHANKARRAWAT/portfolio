bf1c73323804fe830eac1106bddff5ac61b5dabc
Shankar_Rawat <90569452+SHANKARRAWAT@users.noreply.github.com>
2024-01-25 09:56:59 +0530

    update

--- src/components/Work.js
+++ src/components/Work.js
@@ -3,12 +3,25 @@ import './WorkCardStyle.css'
 import React from 'react'
 import news from '../assests/news.jpg'
 import { NavLink } from 'react-router-dom'
+import WorkCard from './WorkCard'
+import WorkCardData from './WorkCardData'
 const Work = () => {
   return (
-
     <div className='work-container'>
         <h1 className="project-heading">Projects</h1>
-      
+         <div className='project-container'>
+          {WorkCardData.map((val,ind)=>{
+            return (
+              <WorkCard key={ind} 
+              imgsrc={val.imgsrc}
+              title={val.title}
+              text={val.text}
+              view={val.view}
+
+              />
+            )
+          })}
+         </div>
     </div>
   )
 }
--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -3,25 +3,22 @@ import './WorkCardStyle.css'
 import React from 'react'
 import news from '../assests/news.jpg'
 import { NavLink } from 'react-router-dom'
-const WorkCard = () => {
+const WorkCard = (props) => {
   return (
 
     <div className="project-container">
-             
     <div className="project-card">
     <img src={props.imgsrc} alt="img not found"  />
         <h2 className='project-title'>
-            project title
+         {props.title}
         </h2>
         <div className="pro-details">
-            <p>this is text</p>
+            <p>{props.text}</p>
       
         <div className="pro-btns">
             <NavLink to="url.com" className="btn">
                  view
              </NavLink>
-
-       
         <NavLink to="url.com" className="btn">source </NavLink>
       </div>
     </div>
